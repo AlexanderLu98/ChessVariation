0bb1d5bc3199b9dff8b4f973809e690fd0210c50
Alexander Lu <alex.lu2710@gmail.com>
2022-12-21 04:49:25 +0100

    need to create a class specific for pieces

--- src/main.py
+++ src/main.py
@@ -1,63 +1,174 @@
 import tkinter as tk
 
-root = tk.Tk()
-root.title("Horde Chess")
+def move_piece(from_x, from_y, to_x, to_y):
+    # update the positions list with the new position of the piece
+    positions[to_y][to_x], positions[from_y][from_x] = positions[from_y][from_x], positions[to_y][to_x]
+    # redraw the board with the updated positions
+    draw_board()
 
-# Create the canvas
-canvas = tk.Canvas(root, width=400, height=400)
+# function to handle when a piece is clicked
+def on_piece_click(event):
+    global from_x, from_y, to_x, to_y
+    # get the coordinates of the click event
+    to_x, to_y = (event.x // SQUARE_SIZE), (event.y // SQUARE_SIZE)
+    # check if a piece is already selected
+    if from_x is not None and from_y is not None:
+        # move the piece
+        move_piece(from_x, from_y, to_x, to_y)
+        # reset the from coordinates
+        from_x = None
+        from_y = None
+    else:
+        # select the piece to be moved
+        from_x, from_y = to_x, to_y
+
+
+# define the size of each square on the board
+SQUARE_SIZE = 60
+
+# create the window
+window = tk.Tk()
+window.title("Chess Board")
+
+# create a canvas to draw the board on
+canvas = tk.Canvas(window, width=8*SQUARE_SIZE, height=8*SQUARE_SIZE)
 canvas.pack()
 
-square_size = 50
+# define the positions of the pieces
+positions = [
+    ('R', 0, 7), ('N', 1, 7), ('B', 2, 7), ('Q', 3, 7), ('K', 4, 7), ('B2', 5, 7), ('N2', 6, 7), ('R2', 7, 7),
+    ('P', 0, 6), ('P2', 1, 6), ('P3', 2, 6), ('P4', 3, 6), ('P5', 4, 6), ('P6', 5, 6), ('P7', 6, 6), ('P8', 7, 6),
+    (' ', 0, 5), (' ', 1, 5), (' ', 2, 5), (' ', 3, 5), (' ', 4, 5), (' ', 5, 5), (' ', 6, 5), (' ', 7, 5),
+    (' ', 0, 4), (' ', 1, 4), (' ', 2, 4), (' ', 3, 4), (' ', 4, 4), (' ', 5, 4), (' ', 6, 4), (' ', 7, 4),
+    (' ', 0, 3), (' ', 1, 3), (' ', 2, 3), (' ', 3, 3), (' ', 4, 3), (' ', 5, 3), (' ', 6, 3), (' ', 7, 3),
+    (' ', 0, 2), (' ', 1, 2), (' ', 2, 2), (' ', 3, 2), (' ', 4, 2), (' ', 5, 2), (' ', 6, 2), (' ', 7, 2),
+    ('p', 0, 1), ('p2', 1, 1), ('p3', 2, 1), ('p4', 3, 1), ('p5', 4, 1), ('p6', 5, 1), ('p7', 6, 1), ('p8', 7, 1),
+    ('r', 0, 0), ('n', 1, 0), ('b', 2, 0), ('q', 3, 0), ('k', 4, 0), ('b2', 5, 0), ('n2', 6, 0), ('r2', 7, 0)
+]
+# create global variables to track the position of the piece being moved
+from_x = None
+from_y = None
+to_x = None
+to_y = None
 
-# Draw the board
-for row in range(8):
-    for col in range(8):
-        x1 = col * square_size
-        y1 = row * square_size
-        x2 = x1 + square_size
-        y2 = y1 + square_size
-        color = "white" if (row + col) % 2 == 0 else "black"
-        canvas.create_rectangle(x1, y1, x2, y2, fill=color)
+def draw_board():
+    # clear the canvas
+    canvas.delete("all")
+# draw the squares of the board
+    for i in range(8):
+        for j in range(8):
+            x1 = i * SQUARE_SIZE
+            y1 = j * SQUARE_SIZE
+            x2 = x1 + SQUARE_SIZE
+            y2 = y1 + SQUARE_SIZE
+            color = 'white' if (i+j) % 2 == 0 else 'lightgrey'
+            canvas.create_rectangle(x1, y1, x2, y2, fill=color)
 
-# Draw a white piece
-x = 2 * square_size
-y = 6 * square_size
-piece = canvas.create_oval(x + 10, y + 10, x + 40, y + 40, fill="white")
+# create the pieces
+    for piece, i, j in positions:
+        x = i * SQUARE_SIZE + SQUARE_SIZE // 2
+        y = j * SQUARE_SIZE + SQUARE_SIZE // 2
 
-# Event handlers
-selected_piece = None
+        if piece == 'R':
+            white_rook_img = tk.PhotoImage(file='src\images\Rook_white.png')
+            canvas.create_image(x, y, image=white_rook_img, anchor='center')
+        elif piece == 'N':
+            white_knight_img = tk.PhotoImage(file='src\images\knight_white.png')
+            canvas.create_image(x, y, image=white_knight_img, anchor='center')
+        elif piece == 'B':
+            white_bishop_img = tk.PhotoImage(file='src\images\Bishop_white.png')
+            canvas.create_image(x, y, image=white_bishop_img, anchor='center')
+        if piece == 'R2':
+            white_rook2_img = tk.PhotoImage(file='src\images\Rook_white.png')
+            canvas.create_image(x, y, image=white_rook2_img, anchor='center')
+        elif piece == 'N2':
+            white_knight2_img = tk.PhotoImage(file='src\images\knight_white.png')
+            canvas.create_image(x, y, image=white_knight2_img, anchor='center')
+        elif piece == 'B2':
+            white_bishop2_img = tk.PhotoImage(file='src\images\Bishop_white.png')
+            canvas.create_image(x, y, image=white_bishop2_img, anchor='center')
+        elif piece == 'Q':
+            white_queen_img = tk.PhotoImage(file='src\images\queen_white.png')
+            canvas.create_image(x, y, image=white_queen_img, anchor='center')
+        elif piece == 'K':
+            white_king_img = tk.PhotoImage(file='src\images\king_white.png')
+            canvas.create_image(x, y, image=white_king_img, anchor='center')
+        elif piece == 'P':
+            white_pawn_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn_img, anchor='center')
+        elif piece == 'P2':
+            white_pawn2_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn2_img, anchor='center')
+        elif piece == 'P3':
+            white_pawn3_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn3_img, anchor='center')
+        elif piece == 'P4':
+            white_pawn4_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn4_img, anchor='center')
+        elif piece == 'P5':
+            white_pawn5_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn5_img, anchor='center')
+        elif piece == 'P6':
+            white_pawn6_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn6_img, anchor='center')
+        elif piece == 'P7':
+            white_pawn7_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn7_img, anchor='center')
+        elif piece == 'P8':
+            white_pawn8_img = tk.PhotoImage(file='src\images\pawn_white.png')
+            canvas.create_image(x, y, image=white_pawn8_img, anchor='center')
+        elif piece == 'p':
+            pawn_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn_img, anchor='center')
+        elif piece == 'p2':
+            pawn2_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn2_img, anchor='center')
+        elif piece == 'p3':
+            pawn3_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn3_img, anchor='center')
+        elif piece == 'p4':
+            pawn4_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn4_img, anchor='center')
+        elif piece == 'p5':
+            pawn5_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn5_img, anchor='center')
+        elif piece == 'p6':
+            pawn6_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn6_img, anchor='center')
+        elif piece == 'p7':
+            pawn7_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn7_img, anchor='center')
+        elif piece == 'p8':
+            pawn8_img = tk.PhotoImage(file='src\images\pawn_black.png')
+            canvas.create_image(x, y, image=pawn8_img, anchor='center')
+        elif piece == 'r':
+            rook_img = tk.PhotoImage(file='src\images\Rook_black.png')
+            canvas.create_image(x, y, image=rook_img, anchor='center')
+        elif piece == 'n':
+            knight_img = tk.PhotoImage(file='src\images\knight_black.png')
+            canvas.create_image(x, y, image=knight_img, anchor='center')
+        elif piece == 'b':
+            bishop_img = tk.PhotoImage(file='src\images\Bishop_black.png')
+            canvas.create_image(x, y, image=bishop_img, anchor='center')
+        elif piece == 'r2':
+            rook2_img = tk.PhotoImage(file='src\images\Rook_black.png')
+            canvas.create_image(x, y, image=rook2_img, anchor='center')
+        elif piece == 'n2':
+            knight2_img = tk.PhotoImage(file='src\images\knight_black.png')
+            canvas.create_image(x, y, image=knight2_img, anchor='center')
+        elif piece == 'b2':
+            bishop2_img = tk.PhotoImage(file='src\images\Bishop_black.png')
+            canvas.create_image(x, y, image=bishop2_img, anchor='center')
+        elif piece == 'q':
+            queen_img = tk.PhotoImage(file='src\images\queen_black.png')
+            canvas.create_image(x, y, image=queen_img, anchor='center')
+        elif piece == 'k':
+            king_img = tk.PhotoImage(file='src\images\king_black.png')
+            canvas.create_image(x, y, image=king_img, anchor='center')
+    
+draw_board()
+# bind an event to the canvas to detect when a piece is clicked
+canvas.bind("<Button-1>", on_piece_click)
 
-def on_piece_press(event):
-    global selected_piece
-    x, y = event.x, event.y
-    selected_piece = canvas.find_withtag("current")[0]
-
-def on_piece_drag(event):
-    x, y = event.x, event.y
-    canvas.coords(selected_piece, x - square_size / 2, y - square_size / 2,
-                         x + square_size / 2, y + square_size / 2)
-
-def on_piece_release(event):
-    global selected_piece
-    x, y = event.x, event.y
-    canvas.coords(selected_piece, x - square_size / 2, y - square_size / 2,
-                         x + square_size / 2, y + square_size / 2)
-    selected_piece = None
-
-# Bind events to the canvas
-canvas.bind_all("<Button-1>", on_piece_press)
-canvas.bind_all("<B1-Motion>", on_piece_drag)
-canvas.bind_all("<ButtonRelease-1>", on_piece_release)
-
-# Create color select buttons
-color = tk.StringVar()
-
-white_button = tk.Radiobutton(root, text="White", variable=color, value="white")
-white_button.pack()
-
-black_button = tk.Radiobutton(root, text="Black", variable=color, value="black")
-black_button.pack()
-
-# To get the chosen color, use `color.get()`
-
-root.mainloop()
+# start the event loop
+window.mainloop()
